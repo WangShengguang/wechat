2d43d8c49e4a5e45e0e7fda0170a0a4fb9eeb21b
WSG-PC\wsg <wsg@shujujiance.com>
2017-01-12 00:47:38 +0800

    init

---
+++ app/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.pagedown import PageDown
+from config import config
+
+from flask.ext.whooshalchemyplus import whoosh_index
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+# db = SQLAlchemy()
+pagedown = PageDown()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+login_manager.login_message = "进行此操作前需要登录账号."
+
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    # app.config.from_object(config[config_name])
+    # config[config_name].init_app()
+    # # 以上为原配置,下为采用本地配置
+    app.config.from_object(config)
+    app.config.from_pyfile('config.py')  # 是否启用instance的配置
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    # db.init_app(app)
+    login_manager.init_app(app)
+    pagedown.init_app(app)
+    from .main import main as main_blueprint
+
+    app.register_blueprint(main_blueprint, url_prefix='/wechat')  ## 如果使用了这个参数，注册后蓝本中定义的 所有路由都会加上指定的前缀，即这个例子中的 /wechat
+
+
+    return app
---
+++ config.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+from setting_local import TOKEN
+
+
+class Config:
+    token = TOKEN
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guss string.'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = '1085092799@qq.com'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = os.environ.get('FLASKY_POSTS_PER_PAGE') or 20
+
+    # whoosh索引数据库位置
+    # WHOOSH_BASE = 'mysql://root:password@localhost/search.db'
+
+    @staticmethod
+    def init_app():
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '1085092799@qq.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
